58e6cb6d1193bc011d88f151e9d08cdcd4df8322
kccjay <cchave3462@greenvilleschools.us>
2017-08-31 09:19:25 -0400

    Update and rename herro.py to madlib.py

---
+++ madlib.py
@@ -0,0 +1,26 @@
+# This program says hello and greets a person by name
+#
+# Carlitos chavez-knotts
+# August 24, 2017
+
+
+print("Put noun?")
+noun = input()
+print("Put a place?")
+place = input()
+print("What's a thing you like")
+thing = input()
+print("
+print( noun + " went to " + place + " to get some " + thing + ".")
+
+#running = True
+#
+#while running:
+#      print("Tell me something about your day so far")
+#      something = input()
+
+#      if something == "goodbye":
+#          running = False
+#      elif "bad" or "I know" in something:
+#          print ("Awww I feel bad is there a way I can help?")
+
